d23996521599b706491f177b7a0610d92b4b26ff
vertesip <hello@vertesipatrik.com>
2022-06-16 19:18:49 +0200

    Commented out cloud function for test

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+/* const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt-start');
 
 const nuxtConfig = require('./nuxt.config.js');
@@ -14,4 +14,4 @@ const nuxt = new Nuxt(config);
 exports.ssrapp = functions.https.onRequest(async (req, res) => {
     await nuxt.ready();
     nuxt.render(req, res);
-});
+}); */
