441581b2f6ff307587f9af5a049707a109653c8c
vertesip <hello@vertesipatrik.com>
2022-06-17 18:15:56 +0200

    Modified deployment

--- src/.nuxt/firebase/service.firestore.js
+++ src/.nuxt/firebase/service.firestore.js
@@ -1,7 +1,7 @@
 
 
 export default async function (session) {
-  await import(/* webpackChunkName: 'firebase-firestore' */'firebase/compat/firestore')
+  await import('firebase/compat/firestore')
 
   const firestoreService = session.firestore()
 
