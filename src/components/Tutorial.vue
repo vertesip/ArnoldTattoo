a43f39ab45416120c4d54cea7f3ba2afa57cd032
vertesip <hello@vertesipatrik.com>
2022-06-17 15:25:20 +0200

    Modified deployment

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -73,7 +73,7 @@
           framework.<br />
         </p>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
-          Hello Firebase 3 modified
+          asdasdasdasdasdsadasdasdasdasd
         </p>
       </div>
       <div class="flex justify-center pt-4 space-x-2">
