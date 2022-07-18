f387f88b773919fa5fac515999eed699ad0aaa19
vertesip <hello@vertesipatrik.com>
2022-07-18 21:03:12 +0200

    Added colored google map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,12 +1,18 @@
 <template>
   <section id="googleMap">
     <GmapMap
-      :center="myCoordinates"
-      :zoom="zoom"
+      :center="ShopCoordinates"
+      :zoom="15"
+      :options="mapStyle"
       style="width: 100%; height: 400px"
       ref="mapRef"
       @dragend="handleDrag"
-    ></GmapMap>
+    >
+      <gmap-marker
+        :position="ShopCoordinates"
+        @click="center = ShopCoordinates"
+      ></gmap-marker>
+    </GmapMap>
   </section>
 </template>
 
@@ -15,11 +21,276 @@ export default {
   data() {
     return {
       map: null,
-      myCoordinates: {
-        lat: 47.4979,
-        lng: 19.0402,
+      ShopCoordinates: {
+        lat: 47.74295,
+        lng: 18.1253163,
       },
       zoom: 6,
+      mapStyle: {
+        styles: [
+          {
+            featureType: "all",
+            elementType: "labels",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "all",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                saturation: 36,
+              },
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 40,
+              },
+            ],
+          },
+          {
+            featureType: "all",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                visibility: "on",
+              },
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 16,
+              },
+            ],
+          },
+          {
+            featureType: "all",
+            elementType: "labels.icon",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "administrative",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 20,
+              },
+            ],
+          },
+          {
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 17,
+              },
+              {
+                weight: 1.2,
+              },
+            ],
+          },
+          {
+            featureType: "administrative.country",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#aa923b",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#c4c4c4",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.neighborhood",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#e5c163",
+              },
+            ],
+          },
+          {
+            featureType: "landscape",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 20,
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 21,
+              },
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "poi.business",
+            elementType: "geometry",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#aa923b",
+              },
+              {
+                lightness: "0",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#000000",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#e5c163",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 18,
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#575757",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#ffffff",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#2c2c2c",
+              },
+            ],
+          },
+          {
+            featureType: "road.local",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 16,
+              },
+            ],
+          },
+          {
+            featureType: "road.local",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#999999",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 19,
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+              {
+                lightness: 17,
+              },
+            ],
+          },
+        ],
+      },
     };
   },
   created() {
@@ -29,7 +300,7 @@ export default {
       } else {
         this.$getLocation({})
           .then((coordinates) => {
-            this.zoom = 10;
+            this.zoom = 5;
             this.myCoordinates = coordinates;
           })
           .catch((error) => console.log(error));
@@ -60,8 +331,8 @@ export default {
     mapCoordinates() {
       if (!this.map) {
         return {
-          lat: 47.4979,
-          lng: 19.0402,
+          lat: 47.74295,
+          lng: 18.1253163,
         };
       }
       return {
