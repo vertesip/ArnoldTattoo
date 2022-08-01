ab7373f3645c6162e740024b449ace11d25251e9
vertesip <hello@vertesipatrik.com>
2022-08-01 22:02:49 +0200

    Fixed section background images

--- src/components/ShirtsSection.vue
+++ src/components/ShirtsSection.vue
@@ -1,22 +1,7 @@
 <template>
   <section
     id="shirts"
-    style="
-      background-image: linear-gradient(
-          to bottom,
-          rgba(13, 13, 13, 1),
-          rgba(13, 13, 13, 0),
-          rgba(13, 13, 13, 1)
-        ),
-        linear-gradient(
-          to top,
-          rgba(13, 13, 13, 1),
-          rgba(13, 13, 13, 0.8),
-          rgba(13, 13, 13, 1)
-        ),
-        url('ship.jpeg');
-    "
-  >
+    style="background-image: linear-gradient( to bottom, rgba(13,13,13,1), rgba(13,13,13,0), rgba(13,13,13,1) ),linear-gradient( to top, rgba(13,13,13,1), rgba(13,13,13,0.8), rgba(13,13,13,1) ),url('ship.jpeg');">
     <div
       class="flex max-w-7xl mx-auto p-4 flex-wrap justify-around my-20 gap-3"
     >
